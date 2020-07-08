db66d000e18863ae3a16d01d1f81209d03e8a9d3
Willem Meijer <willem1998m@hotmail.com>
2020-07-08 18:45:41 +0200

    Added Menu Screen
    Added libraries: SimpleJson & Windows.Forms
    Added menu screen scene that can load and create new projects.

---
+++ Interactive-Roleplaying-Map/Assets/Scripts/Management/BasicSettings.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class BasicSettings : MonoBehaviour
+{
+	public static BasicSettings instance; 
+
+	public string Name;
+	public string StoragePath;
+	public string ImagePath;
+	
+	public void Start()
+	{
+		if (instance == null)
+		{
+			DontDestroyOnLoad(this);
+			instance = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	public void Set(string name, string storagePath, string imagePath)
+	{
+		this.Name = name;
+		this.StoragePath = storagePath;
+		this.ImagePath = imagePath;
+	}
+
+	public void LoadSettingsFrom(string path)
+	{
+
+	}
+}
---
+++ Interactive-Roleplaying-Map/Assets/Scripts/Menu/Load.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Windows.Forms;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public Scenes EditorScene;
+
+	public void OnEnable()
+	{
+        using (OpenFileDialog fileBrowser = new OpenFileDialog())
+        {
+            fileBrowser.Filter = @"Interactive Roleplaying Map|*.irm;*.IRM";
+            DialogResult result = fileBrowser.ShowDialog();
+            string path = fileBrowser.FileName;
+
+            if (result == DialogResult.OK
+                && File.Exists(path))
+            {
+                BasicSettings.instance.LoadSettingsFrom(path);
+                SceneManager.LoadScene((int)EditorScene);
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
