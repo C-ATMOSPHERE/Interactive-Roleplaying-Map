614ff448f306b39e9427f91dacc9682abb85ff0c
Willem Meijer <willem1998m@hotmail.com>
2020-07-13 17:18:15 +0200

    .irm files can be loaded with program.

---
+++ Interactive-Roleplaying-Map/Assets/Scripts/Menu/ProjectAutoLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProjectAutoLoader : MonoBehaviour
+{
+    public string InvalidFileLoadedMessage;
+    public string InvalidFileLoadedCaption;
+    public Scenes EditorScene;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        string file = GetIrmFile();
+        if (file != null)
+        {
+            if (File.Exists(file))
+            {
+                BasicSettings settings = BasicSettings.Instance;
+                settings.MapFilePath = file;
+                SceneManager.LoadScene((int)EditorScene);
+            }
+            else
+            {
+                MessageBox.Show(InvalidFileLoadedMessage, InvalidFileLoadedCaption);
+            }
+        }
+    }
+
+    private string GetIrmFile()
+    {
+        foreach (string arg in System.Environment.GetCommandLineArgs())
+        {
+            if (Path.GetExtension(arg).ToLower() == ".irm")
+            {
+                return arg;
+            }
+        }
+        return null;
+    }
+}
